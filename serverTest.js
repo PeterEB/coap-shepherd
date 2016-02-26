d40c8a40177e6acbe2cc0ed83b937c25821d77bc
PeterEB <peter.eb9@gmail.com>
2016-02-26 18:36:49 +0800

    npm init

--- serverTest.js
+++ serverTest.js
@@ -1,18 +1,21 @@
 var server = require('./lib/coap-shepherd.js');
 
+var cnode;
+
 server.on('ind', handler);
 
 function handler (msg) {
     console.log(msg.type);
     
     if (msg.type === 'registered') {
-        msg.data.observe('/3303/0/5700').then(function (res) {
-            console.log(res);
+        cnode = msg.data;
+        cnode.observe('/3303/0/5702').then(function (res) {
+            console.log(res.status);
         }, function (err) {
             console.log(err);
         });
     } else if (msg.type === 'notify') {
-        console.log(msg);
+        console.log(msg.data);
     }
     
 }
