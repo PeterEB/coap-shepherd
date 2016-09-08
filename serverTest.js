715ab584bb5331399620c8752b9b3b13298b6404
PeterEB <peter.eb9@gmail.com>
2016-09-08 18:46:03 +0800

    debug messages

--- serverTest.js
+++ serverTest.js
@@ -63,7 +63,7 @@ function errHandler (err) {
 }
 
 function handler (msg) {
-    console.log(msg);
+    // console.log(msg);
     
     if (msg.type === 'devIncoming') {
         cnode = shepherd.find('nodeTest');
@@ -94,13 +94,13 @@ function handler (msg) {
 //         setTimeout(function () { cnode.writeReq('/3303/0', { 5700: 87, 5701: 'F' }, reqHandler); }, 28000);
 
 // // writeAttr test
-//         setTimeout(function () { cnode.writeAttrReq('/3303/0/5700', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 3000);
-//         setTimeout(function () { cnode.writeAttrReq('/3303/0/5701', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 8000);
-//         setTimeout(function () { cnode.writeAttrReq('/3303/0/5702', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 13000);
-//         setTimeout(function () { cnode.writeAttrReq('/3303/0/5703', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 18000);
-//         setTimeout(function () { cnode.writeAttrReq('/3303/0/5704', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 23000);
-//         setTimeout(function () { cnode.writeAttrReq('/3303/0', { 'pmin': 10, 'pmax': 30 }, reqHandler); }, 28000);
-//         setTimeout(function () { cnode.writeAttrReq('/3303', { 'pmin': 10, 'pmax': 30 }, reqHandler); }, 33000);
+//        setTimeout(function () { cnode.writeAttrsReq('/3303/0/5700', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 3000);
+//         setTimeout(function () { cnode.writeAttrsReq('/3303/0/5701', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 8000);
+//         setTimeout(function () { cnode.writeAttrsReq('/3303/0/5702', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 13000);
+//         setTimeout(function () { cnode.writeAttrsReq('/3303/0/5703', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 18000);
+//         setTimeout(function () { cnode.writeAttrsReq('/3303/0/5704', { 'pmin': 10, 'pmax': 30, 'gt': 0 }, reqHandler); }, 23000);
+//         setTimeout(function () { cnode.writeAttrsReq('/3303/0', { 'pmin': 10, 'pmax': 30 }, reqHandler); }, 28000);
+//         setTimeout(function () { cnode.writeAttrsReq('/3303', { 'pmin': 10, 'pmax': 30 }, reqHandler); }, 33000);
 
 // // exec test    
 //         setTimeout(function () { cnode.executeReq('/3303/0/5700', ['Peter', 'world'], reqHandler); }, 5000);
@@ -110,7 +110,7 @@ function handler (msg) {
 //         setTimeout(function () { cnode.executeReq('/3303/0/5704', ['Peter', 'world'], reqHandler); }, 25000);
 
 // // observe test
-        setTimeout(function () { cnode.observeReq('/3303/0/5700', reqHandler); }, 5000);
+//        setTimeout(function () { cnode.observeReq('/3303/0/5700', reqHandler); }, 5000);
 //         setTimeout(function () { cnode.observeReq('/3303/0/5701', reqHandler); }, 10000);
 //         setTimeout(function () { cnode.observeReq('/3303/0/5702', reqHandler); }, 15000);
 //         setTimeout(function () { cnode.observeReq('/3303/0/5703', reqHandler); }, 20000);
@@ -124,6 +124,6 @@ function handler (msg) {
 //         setTimeout(function () { cnode.pingReq(reqHandler); }, 3000);
 
 // // remove test
-//         setTimeout(function () { shepherd.remove('nodeTest'); }, 30000);
+//        setTimeout(function () { shepherd.remove('nodeTest'); }, 10000);
     } 
 }
